4c6baf8bd077650a69162e3ee3613e98d8000617
Aravind104 <ontagodiaravind@gmail.com>
2022-10-28 23:06:35 +0530

    rendering-list

---
+++ src/components/List.jsx
@@ -0,0 +1,44 @@
+import { people } from "./data";
+import { getImageUrl } from './utils.jsx';
+function List(){
+    const chemist=people.filter(person => person.profession==="chemist")
+    const nonChemist=people.filter(person => person.profession !== "chemist")
+    const chemistList=chemist.map(
+        person => 
+        <li>
+            <img
+              src={getImageUrl(person)}
+              alt={person.name}
+            />
+            <p>
+             <b>{person.name}:</b>
+             {' ' + person.profession + ' '}
+             known for {person.accomplishment}
+            </p>
+        </li>
+        )
+    const nonChemistList=nonChemist.map(
+        person =>
+        <li>
+            <img
+              src={getImageUrl(person)}
+              alt={person.name}
+            />
+            <p>
+             <b>{person.name}:</b>
+             {' ' + person.profession + ' '}
+             known for {person.accomplishment}
+            </p>
+        </li>
+    )
+    return(
+        <ul>
+            <h1>Chemist</h1>
+            {chemistList}
+            <h1> Every one else</h1>
+            {nonChemistList}
+        </ul>
+    )
+}
+
+export default List;
